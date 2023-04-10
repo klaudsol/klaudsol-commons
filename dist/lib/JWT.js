81705db9a85118c3ef6025aea90b555ee1d407d9
johnservo <jaservo@up.edu.ph>
2023-04-10 12:09:53 +0800

    Added decodeToken

--- dist/lib/JWT.js
+++ dist/lib/JWT.js
@@ -3,14 +3,14 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.verifyToken = exports.generateToken = void 0;
+exports.verifyToken = exports.generateToken = exports.decodeToken = void 0;
 var _jsonwebtoken = _interopRequireDefault(require("jsonwebtoken"));
 var _TokenExpiredError = _interopRequireDefault(require("../errors/TokenExpiredError"));
 var _JsonWebTokenError = _interopRequireDefault(require("../errors/JsonWebTokenError"));
-var _process$env$KS_SECRE;
+var _ref, _ref2, _process$env$KS_SECRE;
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 // Let's reuse SECRET_COOKIE_PASSWORD instead of creating another one
-var SECRET = (_process$env$KS_SECRE = process.env.KS_SECRET_COOKIE_PASSWORD) !== null && _process$env$KS_SECRE !== void 0 ? _process$env$KS_SECRE : process.env.SECRET_COOKIE_PASSWORD;
+var SECRET = (_ref = (_ref2 = (_process$env$KS_SECRE = process.env.KS_SECRET_COOKIE_PASSWORD) !== null && _process$env$KS_SECRE !== void 0 ? _process$env$KS_SECRE : process.env.SECRET_COOKIE_PASSWORD) !== null && _ref2 !== void 0 ? _ref2 : process.env.KS_SECRET) !== null && _ref !== void 0 ? _ref : process.env.SECRET;
 var generateToken = function generateToken(tokenValues) {
   var token = _jsonwebtoken["default"].sign(tokenValues, SECRET, {
     expiresIn: 14400
@@ -28,4 +28,14 @@ var verifyToken = function verifyToken(token) {
   var decodedToken = _jsonwebtoken["default"].verify(token, SECRET, error);
   return decodedToken;
 };
+
+// Verifies the token, but it wont scream when the token is invalid.
 exports.verifyToken = verifyToken;
+var decodeToken = function decodeToken(token) {
+  var error = function error(err, decoded) {
+    return err ? null : decoded;
+  };
+  var decodedToken = _jsonwebtoken["default"].verify(token, SECRET, error);
+  return decodedToken;
+};
+exports.decodeToken = decodeToken;
--- src/lib/JWT.js
+++ src/lib/JWT.js
@@ -3,7 +3,10 @@ import TokenExpiredError from "../errors/TokenExpiredError";
 import JsonWebTokenError from "../errors/JsonWebTokenError";
 
 // Let's reuse SECRET_COOKIE_PASSWORD instead of creating another one
-const SECRET = process.env.KS_SECRET_COOKIE_PASSWORD ?? process.env.SECRET_COOKIE_PASSWORD;
+const SECRET = process.env.KS_SECRET_COOKIE_PASSWORD ?? 
+               process.env.SECRET_COOKIE_PASSWORD ?? 
+               process.env.KS_SECRET ??
+               process.env.SECRET;
 
 export const generateToken = (tokenValues) => {
     const token = jwt.sign(
@@ -33,3 +36,12 @@ export const verifyToken = (token) => {
 
     return decodedToken;
 };
+
+// Verifies the token, but it wont scream when the token is invalid.
+export const decodeToken = (token) => {
+    const error = (err, decoded) => err ? null : decoded;
+
+    const decodedToken = jwt.verify(token, SECRET, error);
+
+    return decodedToken;
+}
